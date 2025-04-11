06c667865be5868cbaf343885955205b0dacf48d
Yash Raj Kohli <yarajkohli@gmail.com>
2025-04-11 16:25:22 +0530

    Register Fragment Fixes and BackPressHandling

--- app/src/main/java/com/example/pocketpal/SplashActivity/SplashActivty.kt
+++ app/src/main/java/com/example/pocketpal/SplashActivity/SplashActivty.kt
@@ -1,22 +1,34 @@
 package com.example.pocketpal.SplashActivity
 
+import android.annotation.SuppressLint
+import android.content.pm.ActivityInfo
 import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example.pocketpal.R
 import com.example.pocketpal.databinding.ActivitySplashActivtyBinding
 
 class SplashActivty : AppCompatActivity() {
     private lateinit var bind:ActivitySplashActivtyBinding
 
+    @SuppressLint("SourceLockedOrientationActivity")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         bind = DataBindingUtil.setContentView(this,R.layout.activity_splash_activty)
-
-
-
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        val backPressHandling = object:OnBackPressedCallback(true){
+            val navController = findNavController(R.id.splashHost)
+            override fun handleOnBackPressed() {
+                if(!navController.popBackStack()){
+                    finish()
+                }
+            }
+        }
+        onBackPressedDispatcher.addCallback(this,backPressHandling)
     }
-
 }
