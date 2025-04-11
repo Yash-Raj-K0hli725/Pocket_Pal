749552d57715741f973d232b13e3988350a419dd
Yash Raj Kohli <yarajkohli@gmail.com>
2025-04-11 16:25:22 +0530

    Register Fragment Fixes and BackPressHandling

--- app/src/main/java/com/example/pocketpal/SplashActivity/SplashActivty.kt
+++ app/src/main/java/com/example/pocketpal/SplashActivity/SplashActivty.kt
@@ -6,37 +6,37 @@ import android.os.Bundle
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import com.example.pocketpal.R
-import com.example.pocketpal.SplashActivity.data.dataStore.DataStoreManager
-import com.example.pocketpal.SplashActivity.data.repository.UserRepository
 import com.example.pocketpal.databinding.ActivitySplashActivtyBinding
 
-
 class SplashActivty : AppCompatActivity() {
-    private lateinit var bind: ActivitySplashActivtyBinding
-    private lateinit var rvm: RegisterViewModel
+    private lateinit var bind:ActivitySplashActivtyBinding
 
     @SuppressLint("SourceLockedOrientationActivity")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        bind = DataBindingUtil.setContentView(this, R.layout.activity_splash_activty)
-        val dataStore = DataStoreManager(this)
-        val repo = UserRepository(dataStore)
-        rvm = ViewModelProvider(this, RegisterViewModelFactory(repo))[RegisterViewModel::class.java]
-
+        bind = DataBindingUtil.setContentView(this,R.layout.activity_splash_activty)
+        ViewCompat.setOnApplyWindowInsetsListener(bind.splashMain){v,inset->
+            val navBars = inset.getInsets(WindowInsetsCompat.Type.navigationBars()).bottom
+            v.updatePadding(bottom = navBars)
+            inset
+        }
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
-        val backPressHandling = object : OnBackPressedCallback(true) {
+        val backPressHandling = object:OnBackPressedCallback(true){
             val navController = findNavController(R.id.splashHost)
             override fun handleOnBackPressed() {
-                if (!navController.popBackStack()) {
+                if(!navController.popBackStack()){
                     finish()
                 }
             }
         }
-        onBackPressedDispatcher.addCallback(this, backPressHandling)
+        onBackPressedDispatcher.addCallback(this,backPressHandling)
     }
 }
