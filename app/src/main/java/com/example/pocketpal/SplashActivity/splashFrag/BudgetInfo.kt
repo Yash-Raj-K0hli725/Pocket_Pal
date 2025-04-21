7852d03132c3477e3999c057113613f9ae487d3b
Yash Raj Kohli <yarajkohli@gmail.com>
2025-04-21 23:58:01 +0530

    fixed BudgetInfo for crashes and some minor changes in BudgetFrag

--- app/src/main/java/com/example/pocketpal/SplashActivity/splashFrag/BudgetInfo.kt
+++ app/src/main/java/com/example/pocketpal/SplashActivity/splashFrag/BudgetInfo.kt
@@ -6,7 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.example.pocketpal.R
 import com.example.pocketpal.database.Budget
 import com.example.pocketpal.databinding.FragmentBudgetInfoBinding
@@ -20,14 +20,27 @@ class BudgetInfo : Fragment() {
     ): View {
         bind = DataBindingUtil.inflate(inflater, R.layout.fragment_budget_info, container, false)
 
-        bind.btnSave.setOnClickListener {
-            val budget = bind.etBudget.text.toString().toLong()
-            val income = bind.etIncome.text.toString().toLong()
-            val data = Budget(budget, income)
-
-            view?.findNavController()?.navigate(BudgetInfoDirections.actionBudgetInfoToRegisterFragment(data))
-        }
-
         return bind.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bind.btnSave.setOnClickListener {
+            if (checkInputs()) {
+                val budget = bind.etBudget.text.toString().toLong()
+                val income = bind.etIncome.text.toString().toLong()
+                val data = Budget(budget, income)
+
+                findNavController().navigate(
+                    BudgetInfoDirections.actionBudgetInfoToRegisterFragment(
+                        data
+                    )
+                )
+            }
+        }
+    }
+
+    private fun checkInputs(): Boolean =
+        bind.etBudget.text!!.isNotEmpty() && bind.etIncome.text!!.isNotEmpty()
+
 }
