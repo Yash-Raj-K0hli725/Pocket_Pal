81974116fa728755ea68be7601667285331abcb5
Yash Raj Kohli <yarajkohli@gmail.com>
2025-04-27 20:14:20 +0530

    some fixes with charu commit

--- app/src/main/java/com/example/pocketpal/MainActivity/MainActivity.kt
+++ app/src/main/java/com/example/pocketpal/MainActivity/MainActivity.kt
@@ -3,25 +3,20 @@ package com.example.pocketpal.MainActivity
 import android.annotation.SuppressLint
 import android.content.pm.ActivityInfo
 import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updatePadding
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
-import com.example.pocketpal.MainActivity.Fragments.Add
-import com.example.pocketpal.MainActivity.Fragments.ExpenseDetails
-import com.example.pocketpal.MainActivity.Fragments.Home
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.pocketpal.R
-import com.example.pocketpal.database.Expense
 import com.example.pocketpal.database.ExpenseDatabase
 import com.example.pocketpal.database.ExpenseRepository
 import com.example.pocketpal.databinding.ActivityMainBinding
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private lateinit var bind: ActivityMainBinding
@@ -32,45 +27,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         bind = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(bind.bottomNavigationView) { _, insets ->
             val navBars = insets.getInsets(WindowInsetsCompat.Type.navigationBars())
-            v.updatePadding(bottom = navBars.bottom)
+            bind.main.updatePadding(bottom = navBars.bottom)
             insets
         }
-
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
 
         val expenseDatabase = ExpenseDatabase.getDatabase(applicationContext)
         val expenseRepository = ExpenseRepository(expenseDatabase)
-        mainViewModel = ViewModelProvider(this, MainViewModelFactory(expenseRepository))[MainViewModel::class.java]
+        mainViewModel = ViewModelProvider(
+            this,
+            MainViewModelFactory(expenseRepository)
+        )[MainViewModel::class.java]
+        val navController = findNavController(R.id.mainHost)
+        bind.bottomNavigationView.setupWithNavController(navController)
 
-        lifecycleScope.launch(Dispatchers.IO) {
-            expenseDatabase
-                .databaseDao()
-                .insertExpense(
-                    Expense(300, "pleasure", 1744848000000, "cash", "sasti rand ki pilai", 1)
-                )
-        }
-
-        val homeFragment = Home()
-        val addFragment = Add()
-
-        setCurrentFragment(homeFragment)
-        bind.bottomNavigationView.setOnItemSelectedListener {
-            when (it.itemId) {
-                R.id.miHome -> setCurrentFragment(homeFragment)
-                R.id.miAdd -> setCurrentFragment(addFragment)
-                R.id.miProfile -> setCurrentFragment(ExpenseDetails())
+        val onBackPressCallback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (!navController.popBackStack())
+                    finish()
             }
-            true
         }
+        onBackPressedDispatcher.addCallback(this,onBackPressCallback)
 
-//        deleteDatabase("expenseDB")
     }
-
-    private fun setCurrentFragment(fragment: Fragment) =
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.flFragment, fragment)
-            commit()
-        }
 }
